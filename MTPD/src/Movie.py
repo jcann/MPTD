0cece48293d78eebf9c3a0cba1588583f55a8190
jcann <cannj65@gmail.com>
2013-12-23 09:02:11 -0500

    Updated Movie class to set and get movie details and poster path. Details are store in a dictionary using the names tmdbId, title, releaseDate, genre, and cast for the associated values.

--- MTPD/src/Movie.py
+++ MTPD/src/Movie.py
@@ -7,12 +7,120 @@
 
 # History
 # 2013-11-03 JC File creation
+# 2013-12-23 JC Updated Movie class to set and get movie details and poster 
+#               path. Details are store in a dictionary using the names tmdbId,
+#               title, releaseDate, genre, and cast for the associated values.
 
 class Movie:
     
     # Constructor
-    def __init__(self):
-        break
+    def __init__(self, tmdbId = None, title = None, releaseDate = None,
+                 genre = [], cast = [], posterPath = None):
+        self.details = {}
         
+        self.details['tmdbId'] = tmdbId
+        self.details['title'] = title
+        self.details['releaseDate'] = releaseDate
+        self.details['genre'] = genre
+        self.details['cast'] = cast
+        
+        self.posterPath = posterPath
+
+    # Set tmdb id
+    def setId(self, tmdbId):
+        self.details['tmdbId'] = tmdbId
+    
+    # Set movie title
+    def setTitle(self, title):
+        self.details['title'] = title
+
+    # Set movie release date
+    def setReleaseDate(self, releaseDate):
+        self.details['releaseDate'] = releaseDate
+        
+    # Set movie genre
+    def setGenre(self, genre):
+        self.details['genre'] = genre
+        
+    # Set movie cast
+    def setCast(self, cast):
+        self.details['cast'] = cast
+    
+    # Set movie poster path
+    def setPosterPath(self, posterPath):
+        self.posterPath = posterPath
+
+    # Get tmdb id
+    def getId(self):
+        return self.details['tmdbId']
+        
+    # Get movie title
+    def getTitle(self):
+        return self.details['title']
+
+    # Get movie release date
+    def getReleaseDate(self):
+        return self.details['releaseDate']
+    
+    # Get movie genre
+    def getGenre(self):
+        return self.details['genre']
+        
+    # Get movie cast
+    def getCast(self):
+        return self.details['cast']
+    
+    # Get movie poster path
+    def getPosterPath(self):
+        return self.posterPath
+    
+    def getDetails(self):        
+        return self.details
+    
+    def getString(self, delim):
+        movieString = ""
+        
+        for tempDetail in self.details:
+            if isinstance(self.details[tempDetail], list):
+                tmp = self.details[tempDetail]
+                for x in tmp:
+                    movieString += x + delim
+            else:
+                movieString += self.details[tempDetail] + delim
+        
+        return movieString.rstrip(delim)
+    
 if __name__=='__main__':
-    break
+    
+    # Test Default Constructor
+    tempMovie = Movie()
+    tempMovie.setId("550")
+    tempMovie.setTitle("Fight Club")
+    tempMovie.setReleaseDate("1999-10-15")
+    tempMovie.setGenre(['Action', 'Drama', 'Thriller'])
+    tempMovie.setCast(['Edward Norton', 'Brad Pitt'])
+    tempMovie.setPosterPath("/2lECpi35Hnbpa4y46JX0aY3AWTy.jpg")
+    
+    print "*** TEST ***"                  
+    print tempMovie.getTitle()
+    print tempMovie.getReleaseDate()
+    print tempMovie.getGenre()
+    print tempMovie.getCast()
+    print tempMovie.getPosterPath()
+    print tempMovie.getDetails()
+    print tempMovie.getString(';')
+    
+    # Test Constructor
+    tempMovie = Movie("550", "Fight Club", "1999-10-15", 
+                      ['Action', 'Drama', 'Thriller'], 
+                      ['Edward Norton', 'Brad Pitt'],
+                      "/2lECpi35Hnbpa4y46JX0aY3AWTy.jpg")
+    
+    print "\n*** TEST ***"
+    print tempMovie.getTitle()
+    print tempMovie.getReleaseDate()
+    print tempMovie.getGenre()
+    print tempMovie.getCast()
+    print tempMovie.getPosterPath()
+    print tempMovie.getDetails()
+    print tempMovie.getString(';')
