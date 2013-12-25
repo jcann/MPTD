609a7b40e421edc61a9653f68a69b55b74981d8d
jcann <cannj65@gmail.com>
2013-12-25 12:36:11 -0500

    Updated MovieDriver class to read input from keyboard. Added menu selection.

--- MTPD/src/MovieDriver.py
+++ MTPD/src/MovieDriver.py
@@ -8,6 +8,8 @@
 # History
 # 2013-11-03 JC File creation
 # 2013-12-23 JC Updated MovieDriver class to read, query, write movie titles.
+# 2013-12-25 JC Updated MovieDriver class to read input from keyboard. Added 
+#               menu selection.
 
 from Movie import Movie
 from MovieList import MovieList
@@ -19,6 +21,7 @@ import os
 #Constants
 SUCCESS =  "Success"
 FAILURE = "Failure"
+OPTIONS = ['1', '2', '3', '4', '5', '6', '7', 'exit']
 
 class MovieDriver:
     
@@ -30,17 +33,54 @@ class MovieDriver:
         self.failed = []
     
     # Read movie titles from file
-    def readFile(self, file_path):
-        print "Reading movie titles from file..."
+    def readFile(self):
+        f_path = ""
+        while not os.path.isdir(f_path):
+            f_path = raw_input("ENTER FILE PATH: ")
+            
+        print "FILES"
+        print "---------------------------"
+        
+        f_list = os.listdir(f_path)
+        for f_name in f_list:
+            print f_name
+
+        f_name = ""
+        while f_name not in f_list:
+            f_name = raw_input("ENTER FILE NAME: ")
+        
+        print "READING MOVIES TITLES FROM FILE..."
+        
+        file_path = os.path.join(f_path, f_name)
         fd = open(file_path, "r")
         
         for title in fd.readlines():
             self.titles.append(title.rstrip())
         
         fd.close()
+            
+    # Read keyboard input
+    def readInput(self):
+        titleInput = ""
 
+        print "INPUT MOVIE TITLES"        
+        while True:
+            titleInput = raw_input("(end to exit): ")
+            
+            if titleInput == 'end':
+                break
+            
+            self.titles.append(titleInput.rstrip())
+        
+    # Print movie titles        
+    def printTitles(self):
+        print "MOVIE TITLES:"
+        print "---------------------------"
+        for title in self.titles:
+            print title
+        
     # Write movie details to file
-    def writeFile(self):
+    def writeFile(self, toWrite = 'csv'):
         f_path = ""
         while not os.path.isdir(f_path):
             f_path = raw_input("ENTER FILE PATH: ")
@@ -48,7 +88,11 @@ class MovieDriver:
         f_name = raw_input("ENTER FILE NAME: ")
         
         movieWrite = MovieWrite(f_path, f_name, self.movieList)
-        movieWrite.writeCsv()
+        
+        if toWrite == 'csv':
+            movieWrite.writeCsv()
+        else: 
+            movieWrite.writeSql()
     
     # Search movie details
     def searchMovies(self, append = None):
@@ -67,8 +111,37 @@ class MovieDriver:
     
 if __name__=='__main__':
     API_KEY = "447ecf40bf72a3fa6218f3024465a567"
+    movieDriver = MovieDriver(API_KEY)
     
-    testMovieDriver = MovieDriver(API_KEY)
-    testMovieDriver.readFile("C:/Users/cannj/Downloads/dev/01_FILMS.txt") 
-    testMovieDriver.searchMovies("credits")
-    testMovieDriver.writeFile()
+    while True:
+        print "MENU"
+        print "---------------------------"    
+        print "   1: FILE INPUT"
+        print "   2: KEYBOARD INPUT"
+        print "   3: PRINT TITLES"
+        print "   4: SEARCH TITLES"
+        print "   5: SEARCH TITLES W/ CAST"
+        print "   6: WRITE CSV"
+        print "   7: WRITE SQL"
+        print "exit: EXIT"
+        
+        menuInput = ""
+        while menuInput not in OPTIONS:
+            menuInput = raw_input("SELECT AN OPTION: ")
+        
+        if menuInput == 'exit':
+            break
+        elif menuInput == '1':
+            movieDriver.readFile()            
+        elif menuInput == '2':
+            movieDriver.readInput()
+        elif menuInput == '3':
+            movieDriver.printTitles()            
+        elif menuInput == '4':
+            movieDriver.searchMovies()
+        elif menuInput == '5':
+            movieDriver.searchMovies("credits")
+        elif menuInput == '6':
+            movieDriver.writeFile()            
+        elif menuInput == '7':
+            movieDriver.writeFile("sql")
